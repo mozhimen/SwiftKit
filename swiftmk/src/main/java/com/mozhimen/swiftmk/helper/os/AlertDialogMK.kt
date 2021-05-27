905401498a3700f21e4bb1d5b283139580a858dc
Kolin Zhao <58215586+mozhimen@users.noreply.github.com>
2021-05-27 16:35:07 +0800

    527

--- swiftmk/src/main/java/com/mozhimen/swiftmk/helper/os/AlertDialogMK.kt
+++ swiftmk/src/main/java/com/mozhimen/swiftmk/helper/os/AlertDialogMK.kt
@@ -13,18 +13,20 @@ import com.mozhimen.swiftmk.R
  * @Version 1.0
  */
 object AlertDialogMK {
-    private lateinit var alertDialog: AlertDialog
+    private var alertDialog: AlertDialog? = null
 
     fun showLoadingDialog(activity: Activity) {
         alertDialog = AlertDialog.Builder(activity).create()
-        alertDialog.window?.setBackgroundDrawable(ColorDrawable())
-        alertDialog.show()
-        alertDialog.setContentView(R.layout.dialog_loading)
+        alertDialog?.let {
+            it.window?.setBackgroundDrawable(ColorDrawable())
+            it.show()
+            it.setContentView(R.layout.dialog_loading)
+        }
     }
 
     fun dismissLoadingDialog() {
-        if (alertDialog != null && alertDialog.isShowing) {
-            alertDialog.dismiss()
+        alertDialog?.let {
+            if(it.isShowing) it.dismiss()
         }
     }
 }
