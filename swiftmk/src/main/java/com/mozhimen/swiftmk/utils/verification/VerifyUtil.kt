a807a14df1bb0eb0ee63e3fe7b4d57058305bb57
Kolin Zhao <58215586+mozhimen@users.noreply.github.com>
2021-05-27 19:04:44 +0800

    527

--- swiftmk/src/main/java/com/mozhimen/swiftmk/utils/verification/VerifyUtil.kt
+++ swiftmk/src/main/java/com/mozhimen/swiftmk/utils/verification/VerifyUtil.kt
@@ -14,6 +14,23 @@ object VerifyUtil {
     private const val DEFAULT = 0
 
     /**
+     * IP 验证
+     */
+    const val REGEX_IP =
+        "((25[0-5]|2[0-4]\\d|1\\d\\d|[1-9]?\\d)\\.){3}(25[0-5]|2[0-4]\\d|1\\d\\d|[1-9]?\\d)"
+
+    /**
+     * 域名验证
+     */
+    const val REGEX_DOMAIN =
+        "^(?=^.{3,255}$)[a-zA-Z0-9][-a-zA-Z0-9]{0,62}(\\.[a-zA-Z0-9][-a-zA-Z0-9]{0,62})+$"
+
+    /**
+     * 端口号验证
+     */
+    const val REGEX_PORT = "^[-\\+]?[\\d]{1,6}$"
+
+    /**
      * 作用: 校验密码
      * 用法1: "...".isPasswordValid()
      *       "...".isPasswordValid(DEFAULT)
@@ -24,4 +41,22 @@ object VerifyUtil {
             else -> false
         }
     }
+
+    /**
+     * 作用: 正则校验
+     */
+    fun String.RegexValid(degree: String): Boolean {
+        return when (degree) {
+            REGEX_IP -> isIPValid(this)
+            REGEX_DOMAIN -> isDoMainValid(this)
+            REGEX_PORT -> isPortValid(this)
+            else -> false
+        }
+    }
+
+    fun isIPValid(ip: String) = ip.matches(Regex(REGEX_IP))
+
+    fun isDoMainValid(domain: String) = domain.matches(Regex(REGEX_DOMAIN))
+
+    fun isPortValid(port: String) = port.matches(Regex(REGEX_PORT))
 }
