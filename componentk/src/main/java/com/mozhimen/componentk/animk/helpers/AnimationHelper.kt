1ff9bc224763b1dbb981144c189067e9ccc8b1b2
mozhimen <mozhimen@163.com>
2022-10-09 00:30:28 +0800

    1009

--- componentk/src/main/java/com/mozhimen/componentk/animk/helpers/AnimationHelper.kt
+++ componentk/src/main/java/com/mozhimen/componentk/animk/helpers/AnimationHelper.kt
@@ -1,44 +1,71 @@
 package com.mozhimen.componentk.animk.helpers
 
 import android.view.View
-import android.view.animation.Animation
-import android.view.animation.Interpolator
-import android.view.animation.LinearInterpolator
-import android.view.animation.RotateAnimation
+import android.view.animation.*
+import com.mozhimen.basick.utilk.UtilKScreen
+import com.mozhimen.componentk.animk.mos.AnimationMo
 
 object AnimationHelper {
+    private fun <T> genAnimation(animationMo: AnimationMo, animation: T): T where T : Animation {
+        animation.fillAfter = animationMo.fillAfter
+        animation.repeatCount = animationMo.repeatCount
+        animation.repeatMode = animationMo.repeatMode
+        animation.interpolator = animationMo.interpolator
+        animation.duration = animationMo.duration
+        animationMo.listener?.let { animation.setAnimationListener(it) }
+        return animation
+    }
+
     /**
      * 开始旋转
-     * @param view View
+     * @param animationMo AnimationMo
      * @param pivotX Float
      * @param pivotY Float
-     * @param fillAfter Boolean
-     * @param repeatCount Int
-     * @param repeatMode Int
-     * @param interpolator Interpolator
-     * @param duration Long
-     * @param listener AnimationListener?
+     * @return Animation
      */
     @JvmStatic
     fun rotate(
-        view: View,
-        pivotX: Float = view.width / 2f,
-        pivotY: Float = view.height / 2f,
-        fillAfter: Boolean = true,
-        repeatCount: Int = Animation.INFINITE,
-        repeatMode: Int = Animation.RESTART,
-        interpolator: Interpolator = LinearInterpolator(),
-        duration: Long = 1000,
-        listener: Animation.AnimationListener? = null
-    ): Animation {
-        val animation = RotateAnimation(0f, 360f, pivotX, pivotY)
-        animation.fillAfter = fillAfter
-        animation.repeatCount = repeatCount
-        animation.repeatMode = repeatMode
-        animation.interpolator = interpolator
-        animation.duration = duration
-        listener?.let { animation.setAnimationListener(it) }
-        return animation
-        //view.startAnimation(animation)
-    }
+        animationMo: AnimationMo,
+        pivotX: Float = animationMo.view.width / 2f,
+        pivotY: Float = animationMo.view.height / 2f,
+    ): Animation =
+        genAnimation(animationMo, RotateAnimation(0f, 360f, pivotX, pivotY))
+
+    /**
+     * 开始抖动:interpolator: Interpolator = AccelerateDecelerateInterpolator
+     * @param animationMo AnimationMo
+     * @param isShakeY Boolean
+     * @param shakeDistance Float
+     * @return Animation
+     */
+    @JvmStatic
+    fun shake(
+        animationMo: AnimationMo,
+        isShakeY: Boolean = true,
+        shakeDistance: Float = animationMo.view.width.coerceAtLeast(animationMo.view.height) / 2f,
+    ): Animation =
+        genAnimation(animationMo, if (isShakeY) TranslateAnimation(0f, 0f, 0f, shakeDistance) else TranslateAnimation(0f, shakeDistance, 0f, 0f))
+
+
+    /**
+     * 从右边进入:repeatCount: Int = 1,interpolator: Interpolator = AccelerateInterpolator(),
+     * @param animationMo AnimationMo
+     * @return Animation
+     */
+    @JvmStatic
+    fun transInRight(
+        animationMo: AnimationMo
+    ): Animation =
+        genAnimation(animationMo, TranslateAnimation(UtilKScreen.getScreenWidth().toFloat(), 0f, 0f, 0f))
+
+    /**
+     * 从右边退出:repeatCount: Int = 1,interpolator: Interpolator = AccelerateInterpolator(),
+     * @param animationMo AnimationMo
+     * @return Animation
+     */
+    @JvmStatic
+    fun transOutRight(
+        animationMo: AnimationMo
+    ): Animation =
+        genAnimation(animationMo, TranslateAnimation(0f, UtilKScreen.getScreenWidth().toFloat(), 0f, 0f))
 }
