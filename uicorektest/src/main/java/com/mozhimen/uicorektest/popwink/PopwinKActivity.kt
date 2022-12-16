24f7822071dd90c1dd61c71e917187d7f4e9f7a9
mozhimen <mozhimen@163.com>
2022-12-17 02:06:59 +0800

    1217

--- uicorektest/src/main/java/com/mozhimen/uicorektest/popwink/PopwinKActivity.kt
+++ uicorektest/src/main/java/com/mozhimen/uicorektest/popwink/PopwinKActivity.kt
@@ -2,6 +2,7 @@ package com.mozhimen.uicorektest.popwink
 
 import android.content.Context
 import android.graphics.Color
+import android.util.Log
 import android.view.View
 import android.view.animation.Animation
 import com.mozhimen.basick.animk.builder.AnimKBuilder
@@ -26,10 +27,84 @@ class PopwinKActivity : BaseActivityVB<ActivityPopwinkBinding>() {
         _popwin.showPopupWindow()
     }
 
+    private val _popwinTest by lazy { PopwinTest(this, "inited") }
+    fun onPopwinShowTest(view: View) {
+        _popwinTest.showPopupWindow()
+    }
+
     class Popwin(context: Context) : PopwinKLifecycle(context) {
         init {
             setContentView(R.layout.layout_popwin)
             setBackgroundColor(Color.TRANSPARENT)
+        }
+
+        override fun onCreateShowAnimation(): Animation {
+            return AnimKBuilder.asAnimation().add(TranslationType.FROM_TOP_SHOW).build()
+        }
+
+        override fun onCreateDismissAnimation(): Animation {
+            return AnimKBuilder.asAnimation().add(TranslationType.TO_TOP_HIDE).build()
+        }
+    }
+
+    interface IListener {
+        fun onInvoke(log: String)
+    }
+
+    class PopwinTest(
+        context: Context,
+        private val _constructor1: String,
+
+        ) : PopwinKLifecycle(context) {
+        private var _variate1: String = "idel"
+        private val _onClickListener = object : IListener {
+            override fun onInvoke(log: String) {
+                Log.d(TAG, "onInvoke: $log")
+            }
+        }
+
+        init {
+/*            _variate1 = "inited"
+            _variate2 = "inited"
+            _variate3 = "inited"
+            _variate4 = "inited"*/
+            setContentView(R.layout.layout_popwin)
+            setBackgroundColor(Color.TRANSPARENT)
+        }
+
+        override fun onViewCreated(contentView: View) {
+            super.onViewCreated(contentView)
+            Log.d(TAG, "onViewCreated: _constructor1 $_constructor1")
+            Log.d(TAG, "onViewCreated: _variate1 $_variate1")
+            _onClickListener.onInvoke("onViewCreated")
+        }
+
+        override fun onBeforeShow(): Boolean {
+            Log.d(TAG, "onBeforeShow: _constructor1 $_constructor1")
+            Log.d(TAG, "onBeforeShow: _variate1 $_variate1")
+            _onClickListener.onInvoke("onBeforeShow")
+            return super.onBeforeShow()
+        }
+
+        override fun onShowing() {
+            super.onShowing()
+            Log.d(TAG, "onShowing: _constructor1 $_constructor1")
+            Log.d(TAG, "onShowing: _variate1 $_variate1")
+            _onClickListener.onInvoke("onShowing")
+        }
+
+        override fun onBeforeDismiss(): Boolean {
+            Log.d(TAG, "onBeforeDismiss: _constructor1 $_constructor1")
+            Log.d(TAG, "onBeforeDismiss: _variate1 $_variate1")
+            _onClickListener.onInvoke("onBeforeDismiss")
+            return super.onBeforeDismiss()
+        }
+
+        override fun onDismiss() {
+            Log.d(TAG, "onDismiss: _constructor1 $_constructor1")
+            Log.d(TAG, "onDismiss: _variate1 $_variate1")
+            _onClickListener.onInvoke("onDismiss")
+            super.onDismiss()
         }
 
         override fun onCreateShowAnimation(): Animation {
