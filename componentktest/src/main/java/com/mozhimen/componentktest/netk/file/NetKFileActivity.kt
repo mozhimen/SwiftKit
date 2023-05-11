8ba92d3a203bac1f194dafcc6d49c55cc36951cc
Kolin Zhao & Mozhimen <58215586+mozhimen@users.noreply.github.com>
2023-05-11 20:20:37 +0800

    0511

--- componentktest/src/main/java/com/mozhimen/componentktest/netk/file/NetKFileActivity.kt
+++ componentktest/src/main/java/com/mozhimen/componentktest/netk/file/NetKFileActivity.kt
@@ -1,6 +1,8 @@
 package com.mozhimen.componentktest.netk.file
 
+import android.net.Uri
 import android.os.Bundle
+import android.util.Log
 import com.liulishuo.okdownload.DownloadTask
 import com.mozhimen.basick.elemk.activity.bases.BaseActivityVB
 import com.mozhimen.basick.manifestk.cons.CPermission
@@ -11,8 +13,16 @@ import com.mozhimen.componentk.netk.file.download_deprecated.commons.IFileDownlo
 import com.mozhimen.basick.manifestk.permission.ManifestKPermission
 import com.mozhimen.basick.manifestk.permission.annors.APermissionCheck
 import com.mozhimen.basick.manifestk.annors.AManifestKRequire
+import com.mozhimen.basick.utilk.java.io.file.UtilKFile
+import com.mozhimen.basick.utilk.jetpack.lifecycle.UtilKDataBus
 import com.mozhimen.basick.utilk.os.UtilKPath
+import com.mozhimen.componentk.netk.file.download.DOWNLOAD_ENGINE_EMBED
+import com.mozhimen.componentk.netk.file.download.DownloadListener
+import com.mozhimen.componentk.netk.file.download.DownloadRequest
+import com.mozhimen.componentk.netk.file.download.NOTIFIER_HIDDEN
 import com.mozhimen.componentktest.databinding.ActivityNetkFileBinding
+import java.io.File
+import kotlin.coroutines.resume
 
 @AManifestKRequire(
     CPermission.READ_EXTERNAL_STORAGE,
@@ -28,24 +38,69 @@ class NetKFileActivity : BaseActivityVB<ActivityNetkFileBinding>() {
     private val _netKFile by lazy { NetKFile(this) }
     private val _musicUrl = "http://192.168.2.6/construction-sites-images/voice/20221102/176f9197f0694591b16ffd47a0f117fe.wav"
     private val _musicPath by lazy { UtilKPath.Absolute.Internal.getFilesDir() + "/netkfile/music.wav" }
+    private var _downloadRequest: DownloadRequest? = null
+
     private val _fileDownloadSingleListener = object : IFileDownloadSingleListener {
         override fun onComplete(task: DownloadTask) {
-            task.file?.let {
-                AudioK.instance.addAudioToPlayList(MAudioK("0001", it.absolutePath, 0))
-            }
+            Log.d(TAG, "onComplete: path ${task.uri?.path}")
+            Log.d(TAG, "onComplete: isFileExists ${task.uri.path?.let { UtilKFile.isFileExist(it) } ?: "null"}")
+            VB.netkFileBtn1.isClickable = true
         }
 
         override fun onFail(task: DownloadTask, e: Exception?) {
+            e?.printStackTrace()
+            Log.e(TAG, "onFail fail msg: ${e?.message}")
+            VB.netkFileBtn1.isClickable = true
+        }
+    }
 
+    private val _downloadListener = object : DownloadListener {
+        override fun onDownloadStart() {
+            Log.d(TAG, "onDownloadStart")
+        }
+
+        override fun onProgressUpdate(percent: Int) {
+            Log.d(TAG, "onProgressUpdate: percent $percent")
+        }
+
+        override fun onDownloadComplete(uri: Uri) {
+            Log.d(TAG, "onDownloadComplete: path ${uri.path}")
+            Log.d(TAG, "onDownloadComplete: isFileExists ${uri.path?.let { UtilKFile.isFileExist(it) } ?: "null"}")
+            VB.netkFileBtn2.isClickable = true
+        }
+
+        override fun onDownloadFailed(e: Throwable) {
+            e.printStackTrace()
+            Log.e(TAG, "onDownloadFailed fail msg: ${e.message}")
+            VB.netkFileBtn2.isClickable = true
         }
     }
 
     override fun initData(savedInstanceState: Bundle?) {
         ManifestKPermission.initPermissions(this) {
             if (it) {
-                _netKFile.download().singleFileTask().start(_musicUrl, _musicPath, _fileDownloadSingleListener)
                 super.initData(savedInstanceState)
             }
         }
     }
+
+    override fun initView(savedInstanceState: Bundle?) {
+        VB.netkFileBtn1.setOnClickListener {
+            VB.netkFileBtn1.isClickable = false
+            _netKFile.download().singleFileTask().start(_musicUrl, _musicPath, _fileDownloadSingleListener)
+        }
+
+        VB.netkFileBtn2.setOnClickListener {
+            VB.netkFileBtn2.isClickable = false
+            _downloadRequest = createCommonRequest(_musicUrl, _musicPath)
+            _downloadRequest!!.registerListener(_downloadListener)
+            _downloadRequest!!.startDownload()
+        }
+    }
+
+    private fun createCommonRequest(url: String, destApkPathWithName: String): DownloadRequest =
+        DownloadRequest(this, url, DOWNLOAD_ENGINE_EMBED)
+            .setNotificationVisibility(NOTIFIER_HIDDEN)
+            .setShowNotificationDisableTip(false)
+            .setDestinationUri(Uri.fromFile(File(destApkPathWithName)))
 }
