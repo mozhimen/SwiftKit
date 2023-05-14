5ee879d24b5db339f616488c51d7c947c74813d5
Kolin Zhao & Mozhimen <mozhimen@163.com>
2023-05-15 00:21:34 +0800

    0515

--- basick/src/main/java/com/mozhimen/basick/utilk/exts/ExtsSeekBar.kt
+++ basick/src/main/java/com/mozhimen/basick/utilk/exts/ExtsSeekBar.kt
@@ -13,3 +13,15 @@ fun SeekBar.setOnSeekBarChangeObserver(seekBarChangeListener: (progress: Int) ->
         override fun onStopTrackingTouch(seekBar: SeekBar) {}
     })
 }
+
+fun SeekBar.setOnSeekBarFinishObserver(seekBarChangeListener: (progress: Int) -> Unit) {
+    setOnSeekBarChangeListener(object : SeekBar.OnSeekBarChangeListener {
+        override fun onProgressChanged(seekBar: SeekBar, progress: Int, fromUser: Boolean) {}
+
+        override fun onStartTrackingTouch(seekBar: SeekBar) {}
+
+        override fun onStopTrackingTouch(seekBar: SeekBar) {
+            seekBarChangeListener(progress)
+        }
+    })
+}
