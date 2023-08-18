54f0798c8252ea6103029ad69939efd31e3c0fa2
Kolin Zhao & Mozhimen <58215586+mozhimen@users.noreply.github.com>
2023-08-18 19:28:09 +0800

    0818

---
+++ basick/src/main/java/com/mozhimen/basick/elemk/android/bluetooth/CBluetoothDevice.kt
@@ -0,0 +1,17 @@
+package com.mozhimen.basick.elemk.android.bluetooth
+
+import android.bluetooth.BluetoothDevice
+
+
+/**
+ * @ClassName CBluetoothDevice
+ * @Description TODO
+ * @Author Mozhimen & Kolin Zhao
+ * @Date 2023/8/17 11:04
+ * @Version 1.0
+ */
+object CBluetoothDevice {
+    const val ACTION_ACL_DISCONNECTED = BluetoothDevice.ACTION_ACL_DISCONNECTED
+    const val EXTRA_DEVICE = BluetoothDevice.EXTRA_DEVICE
+    const val ACTION_FOUND = BluetoothDevice.ACTION_FOUND
+}
