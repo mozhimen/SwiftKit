a523ab40ec2f3d4247f23b9ecac72f2f0036f88c
Kolin Zhao & Mozhimen <58215586+mozhimen@users.noreply.github.com>
2023-08-20 18:41:06 +0800

    0820

---
+++ basick/src/main/java/com/mozhimen/basick/elemk/android/app/cons/CActivity.kt
@@ -0,0 +1,16 @@
+package com.mozhimen.basick.elemk.android.app.cons
+
+import android.app.Activity
+
+
+/**
+ * @ClassName CActivity
+ * @Description TODO
+ * @Author Mozhimen & Kolin Zhao
+ * @Date 2023/8/20 12:46
+ * @Version 1.0
+ */
+object CActivity {
+    const val RESULT_OK = Activity.RESULT_OK
+    const val RESULT_CANCELED = Activity.RESULT_CANCELED
+}
--- basick/src/test/java/com/mozhimen/basick/utilk/TestUtilKJson.kt
+++ basick/src/test/java/com/mozhimen/basick/utilk/TestUtilKJson.kt
@@ -1,8 +1,8 @@
 package com.mozhimen.basick.utilk
 
 import com.mozhimen.basick.utilk.kotlin.printlog
-import com.mozhimen.basick.utilk.squareup.moshi.json2t
-import com.mozhimen.basick.utilk.squareup.moshi.t2json
+import com.mozhimen.basick.utilk.squareup.moshi.jsonMoshi2t
+import com.mozhimen.basick.utilk.squareup.moshi.t2jsonMoshi
 import org.junit.Test
 
 /**
@@ -20,9 +20,9 @@ class TestUtilKJson {
               "user": "DK234455",
               "pwd": "78e3396c576b4420b31acd20e412c5d9"
             }
-        """.trimIndent().json2t<Bean>().printlog()
+        """.trimIndent().jsonMoshi2t<Bean>().printlog()
 
-        Bean("123", "123").t2json().printlog()
+        Bean("123", "123").t2jsonMoshi().printlog()
     }
 
     data class Bean(
