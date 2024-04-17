3f3fd2b4079cd17900ca12222da47d809279b4dc
Kolin Zhao & Mozhimen <58215586+mozhimen@users.noreply.github.com>
2024-04-17 22:07:28 +0800

    0417

--- xmlk/src/main/java/com/mozhimen/xmlk/bases/BaseLayoutKLinear.kt
+++ xmlk/src/main/java/com/mozhimen/xmlk/bases/BaseLayoutKLinear.kt
@@ -3,6 +3,8 @@ package com.mozhimen.xmlk.bases
 import android.content.Context
 import android.util.AttributeSet
 import android.widget.LinearLayout
+import androidx.annotation.RequiresApi
+import com.mozhimen.basick.elemk.android.os.cons.CVersCode
 import com.mozhimen.xmlk.commons.ILayoutK
 
 /**
@@ -11,6 +13,11 @@ import com.mozhimen.xmlk.commons.ILayoutK
  * @Author Kolin Zhao / Mozhimen
  * @Version 1.0
  */
-abstract class BaseLayoutKLinear @JvmOverloads constructor(context: Context, attrs: AttributeSet? = null, defStyleAttr: Int = 0) :
-    LinearLayout(context, attrs, defStyleAttr), ILayoutK {
+abstract class BaseLayoutKLinear : LinearLayout, ILayoutK {
+
+    @JvmOverloads
+    constructor(context: Context, attrs: AttributeSet? = null, defStyleAttr: Int = 0) : super(context, attrs, defStyleAttr)
+
+    @RequiresApi(CVersCode.V_21_5_L)
+    constructor(context: Context, attrs: AttributeSet, defStyleAttr: Int, defStyleRes: Int) : super(context, attrs, defStyleAttr, defStyleRes)
 }
