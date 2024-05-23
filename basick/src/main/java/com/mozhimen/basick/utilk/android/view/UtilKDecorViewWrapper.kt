783e6180e4509f41b04d7c8744c94e0c7e230360
Kolin Zhao & Mozhimen <58215586+mozhimen@users.noreply.github.com>
2024-05-23 21:24:42 +0800

    0523

--- basick/src/main/java/com/mozhimen/basick/utilk/android/view/UtilKDecorViewWrapper.kt
+++ basick/src/main/java/com/mozhimen/basick/utilk/android/view/UtilKDecorViewWrapper.kt
@@ -1,7 +1,9 @@
 package com.mozhimen.basick.utilk.android.view
 
 import android.app.Activity
+import android.view.View
 import android.view.Window
+import com.mozhimen.basick.elemk.android.view.cons.CView
 import com.mozhimen.basick.utilk.android.util.d
 import kotlin.math.abs
 
@@ -20,9 +22,27 @@ object UtilKDecorViewWrapper {
     //获取DecorView区域高度
     @JvmStatic
     fun getInvisibleHeight(window: Window): Int {
-        val decorView = UtilKWindow.getDecorView(window)
+        val decorView = UtilKDecorView.get(window)
         val outRect = UtilKDecorView.getWindowVisibleDisplayFrame(window)
         val delta = abs(decorView.bottom - outRect.bottom)
         return (if (delta <= UtilKNavigationBar.getHeight() + UtilKStatusBar.getHeight()) 0 else delta).also { ("getInvisibleHeight: " + (decorView.bottom - outRect.bottom)).d(UtilKDecorView.TAG) }
+    }
+
+    @JvmStatic
+    fun applyImmersive(activity: Activity) {
+        applyImmersive(UtilKWindow.get(activity))
+    }
+
+    @JvmStatic
+    fun applyImmersive(window: Window) {
+        UtilKDecorView.applySystemUiVisibility(
+            window,
+            CView.SystemUiFlag.IMMERSIVE_STICKY
+                    or CView.SystemUiFlag.LAYOUT_STABLE
+                    or CView.SystemUiFlag.LAYOUT_HIDE_NAVIGATION
+                    or CView.SystemUiFlag.LAYOUT_FULLSCREEN
+                    or CView.SystemUiFlag.HIDE_NAVIGATION
+                    or CView.SystemUiFlag.FULLSCREEN
+        )
     }
 }
