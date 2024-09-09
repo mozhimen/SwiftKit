2e6f8af13f94c9fd4a169804b537de82b3e34a9c
mozhimen <58215586+mozhimen@users.noreply.github.com>
2024-09-09 21:48:39 +0800

    0909

---
+++ basick/src/main/java/com/mozhimen/basick/helpers/MutableLiveDataStrict.kt
@@ -0,0 +1,29 @@
+package com.mozhimen.basick.helpers
+
+import androidx.lifecycle.MutableLiveData
+
+/**
+ * @ClassName MutableLiveDataStrict
+ * @Description TODO
+ * @Author mozhimen
+ * @Date 2024/9/9
+ * @Version 1.0
+ */
+class MutableLiveDataStrict<T> : MutableLiveData<T> {
+
+    constructor() : super()
+
+    constructor(value: T) : super(value)
+
+    override fun postValue(value: T) {
+        if (value == getValue()) return
+        if (value == null) return
+        super.postValue(value)
+    }
+
+    override fun setValue(value: T) {
+        if (value == getValue()) return
+        if (value == null) return
+        super.setValue(value)
+    }
+}
