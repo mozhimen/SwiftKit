499540008eef83ac0e0b2ad156b4a38116d3ea3c
Kolin Zhao & Mozhimen <58215586+mozhimen@users.noreply.github.com>
2024-10-27 15:57:22 +0800

    1027

--- basick/src/main/java/com/mozhimen/basick/bases/BaseWakeBefDestroyLifecycleObserver.kt
+++ basick/src/main/java/com/mozhimen/basick/bases/BaseWakeBefDestroyLifecycleObserver.kt
@@ -6,6 +6,7 @@ import com.mozhimen.basick.utils.runOnMainThread
 import com.mozhimen.kotlin.lintk.optins.OApiInit_ByLazy
 import com.mozhimen.kotlin.elemk.androidx.lifecycle.commons.IDefaultLifecycleObserver
 import com.mozhimen.kotlin.lintk.optins.OApiCall_BindLifecycle
+import com.mozhimen.kotlin.lintk.optins.OApiCall_BindViewLifecycle
 import com.mozhimen.kotlin.utilk.bases.BaseUtilK
 
 /**
@@ -15,6 +16,7 @@ import com.mozhimen.kotlin.utilk.bases.BaseUtilK
  * @Date 2022/11/21 21:22
  * @Version 1.0
  */
+@OApiCall_BindViewLifecycle
 @OApiCall_BindLifecycle
 @OApiInit_ByLazy
 open class BaseWakeBefDestroyLifecycleObserver : IDefaultLifecycleObserver, BaseUtilK() {
